0dec433bb2099603e18dd48e865e6f962c19c9c7
DanielMinty <83800353+DanielMinty@users.noreply.github.com>
2024-01-01 21:06:14 -0600

    Add files via upload

--- main_menu.gd
+++ main_menu.gd
@@ -1,6 +1,10 @@
 extends Control
 
 
+@onready var popup=$Window
+@onready var pause_menu=$PauseMenu
+var paused=false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,12 +12,30 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
+		
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale=1
+	else:
+		pause_menu.show()
+		Engine.time_scale=0
+	paused=!paused
 
 
 func _on_start_pressed():
-	get_tree().change_scene()#insert the scene into here from filesystem)
+	get_tree().change_scene_to_file("res://gameeeee.tscn")#insert the scene into here from filesystem)
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_tutorial_pressed():
+	popup.show()
+
+
+func _on_window_close_requested():
+	popup.hide()
