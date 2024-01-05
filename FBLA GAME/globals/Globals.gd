d340200478ed2f8da08824905370239f9c3946d4
VG-Fish <112359514+VG-Fish@users.noreply.github.com>
2024-01-05 11:05:35 -0600

    Delete FBLA GAME directory

--- FBLA GAME/globals/Globals.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-
-var stats: Dictionary = {"profit": 0.1, "public_relations": 0.1, "employee_friendliness": 0.1, "product_quality": 0.1, "environment_indicators": 0.1}
-var amount_of_actions: int = 3
-var mode: String
-var days_since_start: int = 0
-enum action_type {placeholder = -1, board = 2, regular = 4}
